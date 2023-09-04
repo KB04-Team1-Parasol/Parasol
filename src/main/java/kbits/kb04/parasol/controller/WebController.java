e7e57ef3131bfafeaef04a527c2f6847dc53852f
HWANGJUNGHYUN <f7817455@naver.com>
2023-09-04 09:30:38 +0900

    feat : 초기 설정

---
+++ src/main/java/kbits/kb04/parasol/controller/WebController.java
@@ -0,0 +1,19 @@
+package kbits.kb04.parasol.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+	@GetMapping("/index")
+	public String hello() {
+		
+		return "index";
+	}
+
+	@GetMapping("/signin")
+	public String signin() {
+		
+		return "signin";
+	}
+}
---
+++ src/test/java/kbits/kb04/parasol/ParasolApplicationTests.java
@@ -0,0 +1,13 @@
+package kbits.kb04.parasol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParasolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
