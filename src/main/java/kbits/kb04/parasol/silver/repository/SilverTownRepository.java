2b9ab78c48eabb02eadb348505f1a9aa4fc9c531
이원규 <85729858+OneK-2@users.noreply.github.com>
2023-09-05 15:34:02 +0900

    fest: 키워드 검색 기능 추가

--- src/main/java/kbits/kb04/parasol/silver/repository/SilverTownRepository.java
+++ src/main/java/kbits/kb04/parasol/silver/repository/SilverTownRepository.java
@@ -1,11 +1,13 @@
 package kbits.kb04.parasol.silver.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import kbits.kb04.parasol.silver.entity.SilverTown;
 
 @Repository
-public interface SilverTownRepository extends JpaRepository<SilverTown, Long> {	
-
+public interface SilverTownRepository extends JpaRepository<SilverTown, Long> {
+	List<SilverTown> findByStName(String stName);
 }
--- src/main/java/kbits/kb04/parasol/silver/service/SilverTownService.java
+++ src/main/java/kbits/kb04/parasol/silver/service/SilverTownService.java
@@ -1,5 +1,25 @@
 package kbits.kb04.parasol.silver.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import kbits.kb04.parasol.silver.entity.SilverTown;
+import kbits.kb04.parasol.silver.repository.SilverTownRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class SilverTownService {
+
+	private final SilverTownRepository silverTownRepository;
+
+	@Transactional
+	public List<SilverTown> serachByKeyword(String keyword) {
+		List<SilverTown> silverTowns = this.silverTownRepository.findByStName(keyword);
+
+		return silverTowns;
+	}
 
 }
