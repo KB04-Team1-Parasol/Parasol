c7c8774a9123466a80d389ff971004f6bc606902
raeunlee <raeun19@naver.com>
2023-09-05 15:32:10 +0900

    feat : finance entity, repository 수정

---
+++ src/main/java/kbits/kb04/parasol/finance/repository/FundRepository.java
@@ -0,0 +1,9 @@
+package kbits.kb04.parasol.finance.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import kbits.kb04.parasol.finance.entity.Fund;
+
+public interface FundRepository extends JpaRepository<Fund, Long>{
+
+}
